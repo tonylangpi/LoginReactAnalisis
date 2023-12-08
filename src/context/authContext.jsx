35dc990323debe6a015a0a0da912d6b045e3bcfb
Choper03 <csutucdeleon@gmail.com>
2023-12-08 14:51:16 -0600

    Arreglos xd

--- src/context/authContext.jsx
+++ src/context/authContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useContext, useState } from 'react';
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const Api = 'http://144.91.81.25:4000/'
+  const Api = 'https://144.91.81.25:4000/'
 
   return (
     <AuthContext.Provider value={{Api}}>
